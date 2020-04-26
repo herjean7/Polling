20dac35d6fc66b7e142dd67273070ec16278f9a2
wong hejun <wonghejun@outlook.com>
2020-04-26 17:43:57 +0800

    Added new .NET Core Web API Project

---
+++ Polling.Infrastructure/Services/DateTimeService/DateTimeService.cs
@@ -0,0 +1,10 @@
+﻿using Polling.Application.Interfaces;
+using System;
+
+namespace Polling.Infrastructure.Services.DateTimeService
+{
+    public class DateTimeService : IDateTime
+    {
+        public DateTime Now => DateTime.Now;
+    }
+}
