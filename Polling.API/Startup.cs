e5dcbc2deca6bb9164b55abed4c966d8c0c6362a
wong hejun <wonghejun@outlook.com>
2020-05-01 00:26:34 +0800

    Improved Folder Structure

--- Polling.API/Startup.cs
+++ Polling.API/Startup.cs
@@ -25,6 +25,7 @@ namespace Polling.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMediatR(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Polling/Common/AuditableEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Polling.Domain.Common
+{
+    public class AuditableEntity
+    {
+        public DateTime LastModified { get; set; }
+        public string LastModifiedBy { get; set; }
+    }
+}
